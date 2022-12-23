3d31c34e7ecc70f9ac605159f4659067088be434
ActualAkshay <99019181+ActualAkshay@users.noreply.github.com>
2022-12-23 07:52:16 +0000

    final

--- utils/CONSTANTS.py
+++ utils/CONSTANTS.py
@@ -6,40 +6,29 @@
 # 3. Citation (owner of the video)
 # 4. Position of image clips in the background. See moviepy reference for more information. (https://zulko.github.io/moviepy/ref/VideoClip/VideoClip.html#moviepy.video.VideoClip.VideoClip.set_position)
 background_options = {
+    "origminecraft": (  # Minecraft
+        "https://www.youtube.com/watch?v=Pt5_GSKIWQM",
+        "ItsIpsn.mp4",
+        "ItsIpsn",
+        lambda t: ("center", 480 + t)
+    ),
     "motor-gta": (  # Motor-GTA Racing
         "https://www.youtube.com/watch?v=vw5L4xCPy9Q",
         "bike-parkour-gta.mp4",
         "Achy Gaming",
-        lambda t: ("center", 480 + t),
+        lambda t: ("center", 480 + t)
     ),
     "rocket-league": (  # Rocket League
         "https://www.youtube.com/watch?v=2X9QGY__0II",
         "rocket_league.mp4",
         "Orbital Gameplay",
-        lambda t: ("center", 200 + t),
+        lambda t: ("center", 200 + t)
     ),
     "minecraft": (  # Minecraft parkour
         "https://www.youtube.com/watch?v=n_Dv4JMiwK8",
         "parkour.mp4",
         "bbswitzer",
-        "center",
-    ),
-    "gta": (  # GTA Stunt Race
-        "https://www.youtube.com/watch?v=qGa9kWREOnE",
-        "gta-stunt-race.mp4",
-        "Achy Gaming",
-        lambda t: ("center", 480 + t),
-    ),
-    "csgo-surf": (  # CSGO Surf
-        "https://www.youtube.com/watch?v=E-8JlyO59Io",
-        "csgo-surf.mp4",
-        "Aki",
-        "center",
-    ),
-    "cluster-truck": (  # Cluster Truck Gameplay
-        "https://www.youtube.com/watch?v=uVKxtdMgJVU",
-        "cluster_truck.mp4",
-        "No Copyright Gameplay",
-        lambda t: ("center", 480 + t),
-    ),
+        "center"
+    )
+
 }
